d69733937ade50af8ded5de47c9217beab66e0ba
autaracharya <145379028+autaracharya@users.noreply.github.com>
2025-01-27 19:41:38 -0500

    Fixed .vs directory issue

---
+++ ECommerceG7_QA_A1Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceG7_QA_A1;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceG7_QA_A1Tests
+{
+    [TestFixture]
+    public class Class1
+    { }
+}
